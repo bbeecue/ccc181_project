583a82c73576091a2c3e62810e0632769aee2c1c
Febe Belvis <gwynfebe@gmail.com>
2024-09-20 15:49:07 +0800

    imported flask-wtf  &  csrfprotect for forms; registered student blueprint

--- ssis_project/__init__.py
+++ ssis_project/__init__.py
@@ -1,26 +1,41 @@
-# 2022-0380/ssis_project/__init__.py
-
+# __init__.py
 from flask import Flask
+from flask_wtf.csrf import CSRFProtect
 from mysql.connector import connect, Error
-from settings import DB_HOST, DB_USER, DB_PASSWORD, DB_NAME
-from .main import main_bp
+from settings import DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_SECRET_KEY
+
 
 def create_app():
     app = Flask(__name__)
+    
+    app.config['SECRET_KEY'] = DB_SECRET_KEY
+    
+    csrf = CSRFProtect(app)
+    
+    app.config.from_mapping(
+        DB_HOST=DB_HOST,
+        DB_USER=DB_USER,
+        DB_PASSWORD=DB_PASSWORD,
+        DB_NAME=DB_NAME,
+    )
 
-    # initialize/establish database connection
+    # establish database connection
     try:
         db = connect(
-            host=DB_HOST,
-            user=DB_USER,
-            password=DB_PASSWORD,
-            database=DB_NAME
+            host=app.config['DB_HOST'],
+            user=app.config['DB_USER'],
+            password=app.config['DB_PASSWORD'],
+            database=app.config['DB_NAME'],
         )
         app.config['db'] = db
     except Error as e:
         print(f"Error connecting to MySQL: {e}")
 
-    # register the blueprints
+    # Register blueprints
+    from .main import main_bp
     app.register_blueprint(main_bp)
+    
+    from .student.student import student_bp
+    app.register_blueprint(student_bp)
 
     return app
