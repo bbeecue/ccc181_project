d18ea383dcfff716c2290461d1a02a3dd5506bb7
Febe Belvis <gwynfebe@gmail.com>
2024-09-30 22:09:58 +0800

    added separate modal html files for student add and edit
    fixed flaskforms (still fixing)

--- ssis_project/student/student.py
+++ ssis_project/student/student.py
@@ -1,13 +1,12 @@
 # student.py
 from flask import Blueprint, render_template, current_app, request, redirect, url_for
-from .student_forms import AddStudentForm  
+from .student_forms import StudentForm  
 
 student_bp = Blueprint('student', __name__, url_prefix='/student')
 
 @student_bp.route('/')
 def student_page():
     db = current_app.config['db']
-    
     cursor = db.cursor()
     cursor.execute("SELECT code, name FROM program")
     programs = cursor.fetchall()
@@ -17,17 +16,17 @@ def student_page():
     
     cursor.close()
 
-    form = AddStudentForm()
+    form = StudentForm()
     form.program.choices = [(program[0], program[1]) for program in programs]
 
     return render_template('student.html', form=form, students=students)
 
 @student_bp.route('/add', methods=['GET', 'POST'])
 def add_student():
-    form = AddStudentForm()
+    form = StudentForm()
     db = current_app.config['db']
     
-    # Populate program choices dynamically
+    # populate program choices dynamically
     cursor = db.cursor()
     cursor.execute("SELECT code, name FROM program")
     programs = cursor.fetchall()
@@ -59,4 +58,37 @@ def add_student():
 
     return render_template('student.html', form=form, programs=programs)
 
+@student_bp.route('/edit/<id>', methods=['GET', 'POST'])
+def edit_student(id):
+    form = StudentForm()
+    db = current_app.config['db']
+
+    cursor = db.cursor()
+    cursor.execute("SELECT code, name FROM program")
+    programs = cursor.fetchall()
+    form.program.choices = [(program[0], program[1]) for program in programs]
+
+    if form.validate_on_submit():
+        id_number = id  # Keep ID unchanged
+        first_name = form.first_name.data
+        last_name = form.last_name.data
+        gender = form.gender.data
+        program = form.program.data
+        year_level = form.year_level.data
+
+        try:
+            cursor.execute("""
+                UPDATE student SET first_name=%s, last_name=%s, gender=%s, program=%s, year_level=%s WHERE id_number=%s
+            """, (first_name, last_name, gender, program, year_level, id_number))
+            db.commit()
+        except Exception as e:
+            db.rollback()
+            print(f"Error: {e}")
+            
+        finally:
+            cursor.close()
+            
+        return redirect(url_for('student.student_page'))
+    
+    return render_template('student.html', form=form, programs=programs)
 
--- ssis_project/student/student_forms.py
+++ ssis_project/student/student_forms.py
@@ -2,12 +2,12 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, RadioField, SelectField, SubmitField
 from wtforms.validators import DataRequired, Length, NumberRange
 
-class AddStudentForm(FlaskForm):
+class StudentForm(FlaskForm):
     id_number_year = StringField('ID Number (Year)', validators=[DataRequired(), Length(min=4, max=4)])
     id_number_unique = StringField('ID Number (Unique)', validators=[DataRequired(), Length(min=4, max=4)])
     first_name = StringField('First Name', validators=[DataRequired()])
     last_name = StringField('Last Name', validators=[DataRequired()])
     gender = RadioField('Gender', choices=[('female', 'Female'), ('male', 'Male'), ('other', 'Other')], validators=[DataRequired()])
-    program = SelectField('Program', choices=[], validators=[DataRequired()])  # Choices will be populated dynamically
+    program = SelectField('Program', choices=[], validators=[DataRequired()])
     year_level = IntegerField('Year Level', validators=[DataRequired(), NumberRange(min=1, max=4)])
-    submit = SubmitField('Add Student')
+    submit = SubmitField('Submit')
