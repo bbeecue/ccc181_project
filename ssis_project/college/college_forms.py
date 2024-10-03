535d76281535b1005db5fb98726a2d25c9370c79
Febe Belvis <gwynfebe@gmail.com>
2024-10-03 10:11:42 +0800

    added flaskform for college page

---
+++ ssis_project/college/college_forms.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired, Regexp
+
+class CollegeForm(FlaskForm):
+    
+    college_code = StringField('College Code', validators=[DataRequired(message="College code is required."),
+            Regexp(r'^[A-Z]+$', message="College code must contain only capital letters.")])
+    
+    college_name = StringField('College Name', validators=[DataRequired(message="College name is required."),
+            Regexp(r'^[A-Za-z]+$', message="College name must contain only letters.")])
+    
